f8d290546a7910a1071be9c5e631c08d410e6f63
mon cv <80108568+chouaibmaghraoui@users.noreply.github.com>
2021-07-26 12:10:39 +0200

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_POST['valider']))
+{
+    if(isset($_POST['user_name']) AND isset($_POST['user_email']) AND isset($_POST['user_message']))
+    {
+        if(!empty($_POST['user_name']) AND !empty($_POST['user_email']) AND !empty($_POST['user_message'])) 
+        {
+            $name=htmlspecialchars($_POST['user_mane']);
+            $email=htmlspecialchars($_POST['user_email']);
+            $message=htmlspecialchars($_POST['user_message']);
+
+              
+            echo"<h2> bonjour <mark><b>$name <b> <mark>merci pour votre email:<mark><b> $email <b><mark>,
+            naus avous lu votre message : </br></br> $message</h2>";
+        }
+    }
+}
+?>
